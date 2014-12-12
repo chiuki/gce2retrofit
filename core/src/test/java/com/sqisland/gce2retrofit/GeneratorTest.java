b99bc5847f52bf99a79321d16cf43295f97e5871
Chiu-Ki Chan <chiuki@gmail.com>
2014-12-11 17:26:07 -0700

    Add helloworld tests to GeneratorTest

--- core/src/test/java/com/sqisland/gce2retrofit/GeneratorTest.java
+++ core/src/test/java/com/sqisland/gce2retrofit/GeneratorTest.java
@@ -2,7 +2,6 @@ package com.sqisland.gce2retrofit;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.Exception;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -18,13 +17,33 @@ import static com.google.common.truth.Truth.assertThat;
 @RunWith(JUnit4.class)
 public final class GeneratorTest {
   @Test
-  public void testStringModel() throws Exception {
+  public void testHelloGreetingSync() throws IOException, URISyntaxException {
+    doTestHelloGreeting(EnumSet.of(Generator.MethodType.SYNC), ".sync");
+  }
+
+  @Test
+  public void testHelloGreetingAsync() throws IOException, URISyntaxException {
+    doTestHelloGreeting(EnumSet.of(Generator.MethodType.ASYNC), ".async");
+  }
+
+  @Test
+  public void testHelloGreetingBoth() throws IOException, URISyntaxException {
+    doTestHelloGreeting(EnumSet.allOf(Generator.MethodType.class), ".both");
+  }
+
+  private void doTestHelloGreeting(EnumSet<Generator.MethodType> methodTypes, String suffix)
+      throws IOException, URISyntaxException {
     InputStreamReader reader = new InputStreamReader(
-        GeneratorTest.class.getResourceAsStream("/string-model/discovery.json"));
+        GeneratorTest.class.getResourceAsStream("/helloworld/discovery.json"));
     StringWriterFactory factory = new StringWriterFactory();
-    Generator.generate(reader, factory, null, EnumSet.of(Generator.MethodType.SYNC));
+    Generator.generate(reader, factory, null, methodTypes);
+
     assertThat(factory.getString("com/appspot/example/model/HelloGreeting.java"))
-        .isEqualTo(getExpectedString("/string-model/HelloGreeting.java.expected"));
+        .isEqualTo(getExpectedString("/helloworld/HelloGreeting.java.model"));
+    assertThat(factory.getString("com/appspot/example/model/HelloGreetingCollection.java"))
+        .isEqualTo(getExpectedString("/helloworld/HelloGreetingCollection.java.model"));
+    assertThat(factory.getString("com/appspot/example/Greetings.java"))
+        .isEqualTo(getExpectedString("/helloworld/Greetings.java" + suffix));
   }
 
   private static String getExpectedString(String path) throws URISyntaxException, IOException {
